0fba31c35c8a91e8b8e357c87376aa6ae60c3d07
Shanmukh-g-27 <83949957+Shanmukh-Garlapati-au28@users.noreply.github.com>
2023-02-10 16:19:08 +0530

    Create vowel.c

---
+++ basic/vowel.c
@@ -0,0 +1,13 @@
+//	C program to check given character is vowel or consonant
+#include <stdio.h>
+main(){
+    char k;
+    printf ("enter the value of car");
+    scanf ("%c",&k);
+    if (k=='a'||k=='e'||k=='i'||k=='o'||k=='u'){
+        printf ("true");
+    }
+    else {
+        printf ("flase");
+    }
+}
