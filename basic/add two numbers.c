2ae5521eb91ea34959eec690e3d4236b25dbfd8c
Shanmukh-g-27 <83949957+Shanmukh-Garlapati-au28@users.noreply.github.com>
2023-02-10 16:22:10 +0530

    Create add two numbers.c

---
+++ basic/add two numbers.c
@@ -0,0 +1,14 @@
+//4)	C program to add two numbers without using addition operator
+
+#include<stdio.h>
+main(){
+    int a,b;
+    printf("enter the value");
+    scanf("%d",&a);
+    printf("enter b value");
+    scanf("%d",&b);
+    while(b--){
+        a++;
+    }
+    //printf("%d",a);
+} 
