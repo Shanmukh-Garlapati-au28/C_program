0115fb72eefe4707e62d8a8dca73c3dc84301e20
Shanmukh-g-27 <83949957+Shanmukh-Garlapati-au28@users.noreply.github.com>
2023-02-10 16:23:35 +0530

    Create Circumference.c

---
+++ basic/Circumference.c
@@ -0,0 +1,11 @@
+
+//Find the Area and Circumference of a Circle
+# include <stdio.h>
+main(){
+    int radius;
+    float circle;
+    printf ("enter the radius value");
+    scanf ("%d",&radius);
+    circle = 3.14*(radius*radius);
+    printf ("%f",circle);
+}
